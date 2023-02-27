5ff06302dab17de939862e90461689d910c19755
Yuri <snnp.ad.pnts@gmail.com>
2023-02-26 16:41:51 -0800

    Add projects section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Footer from '../Components/Footer'
 
 import Header from '../Components/Header'
+import ProjectsSection from '../Components/ProjectsSection'
 import lightModeImg from '../images/blue-sky.jpg'
 import darkModeImg from '../images/night-sky.jpg'
 
@@ -9,16 +10,16 @@ const Home = () => {
   const [mode, setMode] = useState(false)
 
   return (
-    <div className='text-white font-poorStory'>
+    <div className='text-white font-poorStory h-fit'>
       <div
         style={{ backgroundImage: mode? `url(${darkModeImg})`: `url(${lightModeImg})` }}
         className={mode
-        ? "bg-cover blur-none -z-50 h-screen w-screen absolute"
-        : "bg-cover blur-sm -z-50 h-screen w-screen absolute"
+        ? "bg-cover blur-none -z-50 h-full w-screen fixed"
+        : "bg-cover blur-sm -z-50 h-full w-screen fixed"
         }        
       ></div>
       <Header setMode={setMode} mode={mode}/>
-
+      <ProjectsSection />
 
       <Footer />
     </div>
